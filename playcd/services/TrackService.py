076981c1bd9baa2c393ecafc014f08651989dfc6
Junior_FOX <cjuniorfox@gmail.com>
2025-09-14 18:00:28 -0300

    added debug information to trackService

--- playcd/services/TrackService.py
+++ playcd/services/TrackService.py
@@ -2,6 +2,8 @@ from playcd.domain.PreparedPlayback import PreparedPlayback
 from playcd.libs.CDPlayer import CDPlayer
 from playcd.services.ControlService import ControlService
 from playcd.services.DisplayService import DisplayService
+from playcd.libs.ApiListener import ApiListener
+from playcd.libs.KeyboardListener import KeyboardListener
 from time import sleep
 
 class TrackService:
@@ -10,7 +12,7 @@ class TrackService:
         self.control_service = ControlService(logging)
         self.display_service = DisplayService(logging)
 
-    def _get_command(self, api_listener, keyboard_listener):
+    def _get_command(self, api_listener: ApiListener, keyboard_listener: KeyboardListener) -> str:
         command = api_listener.get_command()
         if not command:
             command = keyboard_listener.get_command()
@@ -43,6 +45,7 @@ class TrackService:
             command = self._get_command(api_listener, keyboard_listener)
 
             if command in ["next", "prev"]:
+                self.logging.debug("TrackService: Received command to go to the %s track.", command)
                 if command == "prev" and cd_player.get_lsn() - track.get_start_lsn() > 150:
                     cd_player.jump(track.get_start_lsn())
                 else:
