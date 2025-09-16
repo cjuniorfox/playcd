e6ea3e6320202f19b55c9880881bee6623398377
Junior_FOX <cjuniorfox@gmail.com>
2025-09-16 15:59:16 -0300

    Creation of CDPlayerCommandsEnum

---
+++ playcd/domain/CDPlayerCommadsEnum.py
@@ -0,0 +1,11 @@
+from enum import StrEnum
+
+class CDPlayerCommandsEnum(StrEnum):
+    REW = "rew"
+    PREV = "prev"
+    STOP = "stop"
+    PLAY = "play"
+    NEXT = "next"
+    FF = "ff"
+    PAUSE = "pause"
+    QUIT = "quit"
