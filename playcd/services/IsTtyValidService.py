5bc2ebecf61fd464300d8f6f895fa60e1588b531
Junior_FOX <cjuniorfox@gmail.com>
2025-09-23 11:40:49 -0300

    check tty valid

--- playcd/services/IsTtyValidService.py
+++ playcd/services/IsTtyValidService.py
@@ -1,15 +1,18 @@
 import logging
+import os
+import sys
+import termios
 
 class IsTtyValidService:
     def __init__(self):
         self.logging = logging.getLogger(__name__)
 
     def execute(self) -> bool:
-        import os
-        import sys
         try:
-            is_tty_valid = os.isatty(sys.stdout.fileno())
+            fd = sys.stdin.fileno()
+            is_tty_valid = os.isatty(fd)
             self.logging.info(f"Is TTY valid: {is_tty_valid}")
+            termios.tcgetattr(fd) # This will raise an error if TTY is not valid
             return is_tty_valid
         except Exception as e:
             self.logging.error(f"Error checking TTY validity: {e}")
